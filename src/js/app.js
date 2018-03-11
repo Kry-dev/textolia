29f385b228af8b9382537086cbfc4f8b296bb57d
kry-dev <yaroslavkryvda@gmail.com>
2018-03-11 18:01:05 +0200

    add news slider

--- src/js/app.js
+++ src/js/app.js
@@ -5,5 +5,37 @@ const bootstrap = require('bootstrap');
 $(document).ready(function() {
 
     $('[data-toggle="collapse"]').collapse('hide');
-    $('.carousel').carousel('pause');
+    $("#newscarousel").slick({
+        slidesToShow: 3,
+        // the magic
+        responsive: [{
+
+            breakpoint: 1170,
+            settings: {
+                slidesToShow: 3,
+                infinite: true
+            }
+
+        },{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                infinite: true
+            }
+        }, {
+            breakpoint: 768,
+            settings: {
+                centerMode: false,
+                slidesToShow: 2
+            }
+        }, {
+
+            breakpoint: 500,
+            settings: {
+                rows: 1,
+                centerMode: false,
+                slidesToShow: 1
+            }
+        }]
+    });
 });
